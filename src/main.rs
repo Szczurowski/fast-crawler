7690ba378a5763e7d59cfff357ebc3047226b4e3
Julian Szczurowski <julian.szczurowski@gmail.com>
2018-11-04 18:57:05 +0100

    Adding Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: i32) -> String {
+    let number = get_number();
+    format!("Hello, {} year old named {}!", age + number, name)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/hello", routes![hello]).launch();
+}
+
+fn get_number() -> i32 {
+    4
 }
